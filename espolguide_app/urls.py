ef62db726178785993556b93d88bce44bbd040e0
soccerdroid <mguarand@espol.edu.ec>
2018-01-07 17:47:04 -0500

    agregando función de endpoint de bloques

--- espolguide_app/urls.py
+++ espolguide_app/urls.py
@@ -1,16 +1,16 @@
-from django.conf.urls import include,urls
+from django.conf.urls import include,url
 from django.contrib import admin
-admin.autodiscover()
+#admin.autodiscover()
 from . import views
 
 #from .viewsets import 
-from rest_framework.routers import DefaultRouter
+#from rest_framework.routers import DefaultRouter
 
-router = DefaultRouter()
+#router = DefaultRouter()
 #router.register(r')
 
 urlpatterns = [
 	url(r'^$',views.principal),
-	url(r'^bloques/',views.obtenerBloques,name='obtenerBloques')
+	url(r'^bloques/',views.obtenerBloques,name='obtenerBloques'),
 ]
 
