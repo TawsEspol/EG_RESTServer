a71278358bf72010e5dce4b86cbbcd859144c864
jgorotiz <jgorotiz@espol.edu.ec>
2018-01-08 20:04:48 -0500

    Arreglando pequeños errores de sintaxis

--- espolguide_app/views.py
+++ espolguide_app/views.py
@@ -62,5 +62,5 @@ def transformarCoordenadas(coord1,coord2):
 	outProj = Proj(init='epsg:4326')
 	x1,y1 = -11705274.6374,4826473.6922
 	x2,y2 = transform(inProj,outProj,x1,y1)
-	print x2,y2
+	print(x2,y2)
 
