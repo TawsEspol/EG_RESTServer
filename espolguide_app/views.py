512eadae448eec6bfd05681c53ada1142cc4dfd3
jgorotiz <jgorotiz@espol.edu.ec>
2018-01-14 21:12:12 -0500

    Servicio nombres/nmbresAlternativos

--- espolguide_app/views.py
+++ espolguide_app/views.py
@@ -34,7 +34,8 @@ def obtener_bloques(request):
             inp = osr.SpatialReference()
             inp.ImportFromEPSG(32717)
             transformation = osr.CoordinateTransformation(inp, wgs84)
-            tupla_transformada = transformation.TransformPoint(tupla[0], tupla[1])
+            tupla_transformada = transformation.TransformPoint(tupla[
+                                                               0], tupla[1])
             # print(tupla[0])
             coordenadas = []
             coordenadas.append(tupla_transformada[1])
@@ -69,13 +70,55 @@ def obtener_informacion_bloques(request):
     return HttpResponse(json.dumps(diccionario), content_type='application/json')
 
 
-'''Funcion para trasformar un sistema de coordenadas a otro'''
+def info_bloque(request, codigo):
+    diccionario = {}
+    # d["type"]="FeatureCollection"
+    lista = []
+    b = Bloques.objects.filter(codigo=codigo)
+    for b1 in b:
+        print(b1.tipo)
+    feature_element = {}
+    feature_element["type"] = "Feature"
+    feature_element["properties"] = {"codigo": b.codigo, "nombre": b.nombre, "unidad": b.unidad,
+                                     "bloque": b.bloque, "tipo": b.tipo, "descripcio": b.descripcio, "area_m2": b.area_m2}
+    geometry = {}
+    geometry["type"] = "Polygon"
+    coordenadas_externa = []
+    coordenadas_media = []
+    rango = len(b.geom[0][0])
+    for i in range(rango):
+        tupla = b.geom[0][0][i]
+        wgs84 = osr.SpatialReference()
+        wgs84.ImportFromEPSG(4326)
+        inp = osr.SpatialReference()
+        inp.ImportFromEPSG(32717)
+        transformation = osr.CoordinateTransformation(inp, wgs84)
+        tupla_transformada = transformation.TransformPoint(tupla[0], tupla[1])
+        # print(tupla[0])
+        coordenadas = []
+        coordenadas.append(tupla_transformada[1])
+        coordenadas.append(tupla_transformada[0])
+        coordenadas_media.append(coordenadas)
+    # print("SE ACABO EL POLIGONO")
+    coordenadas_externa.append(coordenadas_media)
+    geometry["coordinates"] = coordenadas_externa
+    feature_element["geometry"] = geometry
+    lista.append(feature_element)
+    diccionario["features"] = lista
+    diccionario["type"] = "FeatureCollection"
+    return HttpResponse(json.dumps(diccionario), content_type='application/json')
 
 
-def transformar_coordenadas(coord1, coord2):
-    in_proj = Proj(init='epsg:3857')
-    out_proj = Proj(init='epsg:4326')
-    new_coord_x1, new_coord_y1 = coord1, coord2
-    new_coord_x2, new_coord_y2 = transform(
-        in_proj, out_proj, new_coord_x1, new_coord_y1)
-    print(new_coord_x2, new_coord_y2)
+def nombres_bloques(request):
+    diccionario = {}
+    # d["type"]="FeatureCollection"
+    bloques = Bloques.objects.all()
+    for b in bloques:
+        lista = []
+        if b.codigo not in diccionario:
+            diccionario[b.codigo] = set()
+        if b.nombre != "":
+            lista.append(b.nombre)
+        lista.append(b.tipo)
+        diccionario[b.codigo] = lista
+    return HttpResponse(json.dumps(diccionario), content_type='application/json')
