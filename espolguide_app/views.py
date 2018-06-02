5e184a7af3e7f0115d6a0de29249cad5fbef186b
jgorotiz <jgorotiz@espol.edu.ec>
2018-06-02 18:20:32 -0500

    [SPRINT 3] Bugs task #104

--- espolguide_app/views.py
+++ espolguide_app/views.py
@@ -127,15 +127,15 @@ def nombres_bloques(request):
 def show_photo(request, codigo):
     '''Funcion que genera la ruta para la imagen de los bloques '''
     try:
-        bloq= Bloques.objects.get(id=codigo) 
-        nombre = bloq.bloque
+        #bloq= Bloques.objects.get(id=codigo) 
+        nombre = codigo
         response = HttpResponse(content_type="image/jpeg")
         img = Image.open('espolguide_app/img/'+nombre+'/'+nombre+'.JPG')
         img.save(response, 'jpeg')
         return response
     except Exception as e:
-        bloq= Bloques.objects.get(id=codigo) 
-        nombre = bloq.bloque
+        # bloq= Bloques.objects.get(id=codigo) 
+        nombre = codigo
         response = HttpResponse(content_type="image/png")
         img = Image.open('espolguide_app/img/'+"espol"+'/'+"espol"+'.png')
         img.save(response, 'png')
