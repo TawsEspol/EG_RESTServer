2e3116e2a97d97366930105e266153b230f0fee6
MariaBelenGuaranda <mguarand@espol.edu.ec>
2018-06-03 14:18:06 -0500

    [Sprint3]Bugs Tarea #105

--- espolguide_app/admin.py
+++ espolguide_app/admin.py
@@ -1,9 +1,13 @@
 # -*- coding: latin1 -*-
-from django.contrib import admin
-from .models import Bloques
+from django.contrib.gis import admin
+from .models import *
 
 # Register your models here.
 
 admin.site.register(Bloques)
+admin.site.register(Buildings, admin.GeoModelAdmin)
+admin.site.register(Unities)
+
+
 
 
