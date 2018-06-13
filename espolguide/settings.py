427efba21406706eab5d81059f38d0143323bb08
soccerdroid <mguarand@espol.edu.ec>
2018-06-13 15:24:10 -0500

    [Sprint3] Tarea#117

--- espolguide/settings.py
+++ espolguide/settings.py
@@ -128,10 +128,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+if DEBUG:
+	STATIC_URL = '/static/'
+else:
+	STATIC_URL = 'www.espol-guide.espol.edu.ec/assets/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'espolguide_app/static'),
+    '/home/manager/EG_RESTServer/espolguide_app/img',
 )
