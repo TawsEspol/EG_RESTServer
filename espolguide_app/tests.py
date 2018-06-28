d56ab4c383144874659cc4dd77cb566bd0102191
MariaBelenGuaranda <mguarand@espol.edu.ec>
2018-06-28 12:08:00 -0500

    [Sprint 4] Pruebas Tarea #138

--- espolguide_app/tests.py
+++ espolguide_app/tests.py
@@ -1,15 +1,27 @@
 from django.test import TestCase
 from .views import *
-
+from django.test import RequestFactory, TestCase
+from .models import User
+from .views import add_user, token_user, show_photo
 
 # Create your tests here.
 
 class CasoTest(TestCase):
     
-    def test_views(self):
-        """Animals that can speak are correctly identified"""
-        self.assertEqual(info_bloque(primary_key = 1),'{"features": [{"geometry":\
-         {"coordinates": [[[-2.14537420416497, -79.9651225218215]]], "type": "Polygon"}, \
-         "properties": {"nombre": "Decanato", "codigo": "19", "descripcio": "Gobierno de FICT", "tipo":\
-          "Edificio", "unidad": "FICT", "bloque": "19"}, "type": "Feature"}], "type": "FeatureCollection"}')
 
+    def setUp(self):
+        # Every test needs access to the request factory.
+        self.factory = RequestFactory()
+        #self.user = User(username="usuario_prueba")
+
+    def add_user_test(self):
+        # Create an instance of a GET request.
+        request = self.factory.get('/photoBlock/16-C')
+
+        # Recall that middleware are not supported. You can simulate a
+        # logged-in user by setting request.user manually.
+        request.user = self.user
+
+        # Test my_view() as if it were deployed at /customer/details
+        response = show_photo(request)
+        self.assertEqual(response.status_code, 200)
