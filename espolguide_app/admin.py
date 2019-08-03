0da74b5532f55d603c353202312e934afa31f316
soccerdroid <mguarand@espol.edu.ec>
2019-08-02 22:13:54 -0500

    sprint 1 - script para almacenar datos, junto con los datos curados

--- espolguide_app/admin.py
+++ espolguide_app/admin.py
@@ -1,10 +1,18 @@
 from django.contrib.gis import admin
 from .models import Users, Buildings, Unities, Salons
+from leaflet.admin import LeafletGeoAdmin
 
+admin.site.site_header = "ESPOLGuide Admin";
+admin.site.title = "ESPOLGuide Admin";
+
+class BuildingsAdmin(LeafletGeoAdmin):
+	list_display = ('unity_name','name_espol','code_gtsi',)
+	list_filter = ('unity_name',)
+	
+	
 # Register your models here.
-
 admin.site.register(Users)
-admin.site.register(Buildings, admin.GeoModelAdmin)
+admin.site.register(Buildings, BuildingsAdmin)
 admin.site.register(Unities)
 admin.site.register(Salons)
 
